e5646e5a7d98bf58378bbc78532cc1070beb2835
Maxim Talanov <mail@maxtalanov.ru>
2025-03-15 04:28:26 +0300

    inital commit

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: pluginReact,
+      prettier: pluginPrettier,
+    },
+    settings: {
+      react: {
+        version: '19.0.0',
+      },
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
