550b37548604153235959314851a6edceb2ae896
Maxim Talanov <mail@maxtalanov.ru>
2025-03-29 18:22:30 +0300

    feat(3.3): Добавлен роутер конфиг

---
+++ src/shared/config/routerConfig/routerConfig.tsx
@@ -0,0 +1,29 @@
+import { MainPage } from 'pages/MainPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
+import { RouteProps } from 'react-router-dom';
+
+export type AppRoutesProps = RouteProps & {
+  authOnly?: boolean;
+};
+
+export enum AppRoutes {
+  MAIN = 'main',
+  NOT_FOUND = 'not_found',
+}
+
+export const RoutePath: Record<AppRoutes, string> = {
+  [AppRoutes.MAIN]: '/',
+  [AppRoutes.NOT_FOUND]: '*',
+};
+
+export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
+  [AppRoutes.MAIN]: {
+    path: RoutePath[AppRoutes.MAIN],
+    element: <MainPage />,
+  },
+
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath[AppRoutes.NOT_FOUND],
+    element: <NotFoundPage />,
+  },
+};
