a211b49156b916a6ac89ad89434a25cf7cc47fd2
Maxim Talanov <mail@maxtalanov.ru>
2025-04-16 04:26:40 +0300

    feat(9.2): Улучшения storybook
    - layout
    - background
    - decorators

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,11 +1,24 @@
 import type { Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { RouteDecorator } from '../../src/shared/config/storybook/RouteDecorator/RouteDecorator';
 
 const preview: Preview = {
+  decorators: [StyleDecorator, RouteDecorator],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#1e1e1e' },
+        { name: 'light', value: '#ffffff' },
+      ],
+    },
     controls: { expanded: true },
     docs: {
       inlineStories: true,
+      theme: themes.dark,
     },
   },
 };
