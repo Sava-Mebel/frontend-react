aeedecd5cbc61f5b18800093829c300315e2bfd9
Maxim Talanov <mail@maxtalanov.ru>
2025-05-19 15:57:30 +0300

    ref(10.15).1: Настроена конфигурация отрисовки footer - состояние - поведение - стилизация

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,26 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import AppRouter from 'app/provider/routerProvider/ui/AppRouter';
 import { Header } from 'widgets/Header';
 import { Footer } from 'widgets/Footer';
 
 function App() {
+  const { pathname } = useLocation();
+
+  const footerMode = useMemo(() => {
+    return pathname === '/' ? 'fixed' : 'static';
+  }, [pathname]);
+
   return (
     <div className={'app'}>
       <Header />
-      <main className={'app__main'}>
+      <main className={`app__main ${footerMode === 'fixed' ? 'with-fixed-footer' : ''}`}>
         <Suspense fallback={null}>
           <AppRouter />
         </Suspense>
       </main>
-      <Footer />
+      <Footer mode={footerMode} />
     </div>
   );
 }
