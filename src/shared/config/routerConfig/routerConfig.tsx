4e56e8f4ce1f0a8ac3908677477fce5d10ee3481
Maxim Talanov <mail@maxtalanov.ru>
2025-05-21 12:01:08 +0300

    feat(10.16): Правки по маршрутам

--- src/shared/config/routerConfig/routerConfig.tsx
+++ src/shared/config/routerConfig/routerConfig.tsx
@@ -1,8 +1,10 @@
 import { RouteProps } from 'react-router-dom';
 
+import { RenovationPage } from 'pages/RenovationPage';
+import { InteriorDesignPage } from 'pages/InteriorDesignPage';
 import { MainPage } from 'pages/MainPage';
-import { NotFoundPage } from 'pages/NotFoundPage';
 import { CatalogPage } from 'pages/CatalogPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export type AppRoutesProps = RouteProps & {
   authOnly?: boolean;
@@ -10,18 +12,23 @@ export type AppRoutesProps = RouteProps & {
 
 export enum AppRoutes {
   MAIN = 'main',
+  RENOVATION = 'renovation',
+  INTERIOR_DESIGN = 'interior_design',
   CATALOG = 'catalog',
   CATALOG_CARD = 'catalog_card',
 
+  // last
   NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
+  [AppRoutes.RENOVATION]: '/renovation',
+  [AppRoutes.INTERIOR_DESIGN]: '/interior-design',
   [AppRoutes.CATALOG]: '/catalog',
-  [AppRoutes.CATALOG_CARD]: '/catalog/:group_category',
+  [AppRoutes.CATALOG_CARD]: '/catalog/:id',
 
-  // LAST
+  // последний
   [AppRoutes.NOT_FOUND]: '*',
 };
 
@@ -30,16 +37,24 @@ export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
     path: RoutePath.main,
     element: <MainPage />,
   },
+  [AppRoutes.RENOVATION]: {
+    path: RoutePath.renovation,
+    element: <RenovationPage />,
+  },
+  [AppRoutes.INTERIOR_DESIGN]: {
+    path: RoutePath.interior_design,
+    element: <InteriorDesignPage />,
+  },
   [AppRoutes.CATALOG]: {
     path: RoutePath.catalog,
     element: <CatalogPage />,
   },
   [AppRoutes.CATALOG_CARD]: {
     path: RoutePath.catalog_card,
-    element: <CatalogPage />,
+    element: <CatalogPage />, // или отдельная CatalogCardPage
   },
 
-  // LAST
+  // last
   [AppRoutes.NOT_FOUND]: {
     path: RoutePath.not_found,
     element: <NotFoundPage />,
