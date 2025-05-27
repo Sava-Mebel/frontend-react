3215f688b0e5af2c8f94039ea22d52d60697e6c1
Maxim Talanov <mail@maxtalanov.ru>
2025-05-27 18:35:38 +0300

    ref(12.10): Удален элемент advantages и всего наследствинные файлы и данные

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -4,7 +4,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Logotype } from 'shared/ui/Logotype/Logotype';
 import { SocialBar } from 'widgets/SocialBar';
 import LogoIcon from 'shared/assets/logo/footer-logo.svg';
-import { AdvantagesList } from 'widgets/Footer/model/advantages';
 
 import cls from './Footer.module.scss';
 
@@ -16,21 +15,8 @@ interface FooterProps {
 export const Footer = memo((props: FooterProps) => {
   const { className, mode = 'static' } = props;
 
-  const advantages = AdvantagesList.map((advantage) => {
-    const { title, Icon, id } = advantage;
-
-    return (
-      <li className={cls.advantagesItem} key={id}>
-        <Icon className={cls.advantagesIcon} />
-        <span className={cls.advantagesTitle}>{title}</span>
-      </li>
-    );
-  });
-
   return (
     <footer className={classNames(cls.Footer, { [cls[mode]]: mode }, [className])}>
-      <ul className={cls.advantagesList}>{advantages}</ul>
-
       <div className={cls.bottom}>
         <Logotype Logo={LogoIcon} className={cls.logo} />
         <SocialBar />
