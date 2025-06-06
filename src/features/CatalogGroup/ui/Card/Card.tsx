18ef44465044a02dcaf1ee845dde4b530f5f69ae
Maxim Talanov <mail@maxtalanov.ru>
2025-06-06 14:54:14 +0300

    feat(14.6): Добавлен компонент ссылки на карточку

--- src/features/CatalogGroup/ui/Card/Card.tsx
+++ src/features/CatalogGroup/ui/Card/Card.tsx
@@ -1,33 +1,37 @@
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import { Badge, BadgeTypes } from 'shared/ui/Badge/Badge';
+import { AppLink, AppLinkVariant } from 'shared/ui/AppLink';
 
 import cls from './Card.module.scss';
 
 interface CardProps {
   className?: string;
   badge?: BadgeTypes;
+  toPaht?: string;
 }
 
 export const Card = (props: CardProps) => {
-  const { className, badge } = props;
+  const { className, badge, toPaht = '/catalog/kitchen-sets/1' } = props;
 
   const modsBadge: Mods = badge ? { [cls[badge]]: true } : {};
 
   return (
-    <figure className={classNames(cls.Card, {}, [className])}>
-      {badge && (
-        <span className={classNames(cls.badge, modsBadge)}>
-          <Badge mods={badge} />
-        </span>
-      )}
+    <AppLink variant={AppLinkVariant.ROUTE} to={toPaht} className={cls.AppLink}>
+      <figure className={classNames(cls.Card, {}, [className])}>
+        {badge && (
+          <span className={classNames(cls.badge, modsBadge)}>
+            <Badge mods={badge} />
+          </span>
+        )}
 
-      <img
-        className={cls.img}
-        src={'https://www.arboro.de/blog/wp-content/uploads/2010/05/img-tag.jpg'}
-        alt="img"
-      />
-      <figcaption className={cls.title}>Лол njg kjg</figcaption>
-    </figure>
+        <img
+          className={cls.img}
+          src={'https://www.arboro.de/blog/wp-content/uploads/2010/05/img-tag.jpg'}
+          alt="img"
+        />
+        <figcaption className={cls.title}>Лол njg kjg</figcaption>
+      </figure>
+    </AppLink>
   );
 };
 
