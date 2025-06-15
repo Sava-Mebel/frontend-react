9ca1d4f70f99122c98a2487b5991f737d66bc354
Maxim Talanov <mail@maxtalanov.ru>
2025-06-15 17:33:19 +0300

    feat (16.6.7): Задан заголовок страницы

--- src/pages/RenovationPage/ui/RenovationPage.tsx
+++ src/pages/RenovationPage/ui/RenovationPage.tsx
@@ -1,11 +1,14 @@
 import { memo } from 'react';
 
+import { useDocumentTitle } from 'shared/lib/hooks/useDocumentTitle/useDocumentTitle';
+
 interface RenovationPageProps {
   className?: string;
 }
 
 const RenovationPage = memo((props: RenovationPageProps) => {
   const { className } = props;
+  useDocumentTitle('Ремонт квартир под ключ');
 
   return <>PAGE: InteriorDesignPage</>;
 });
