aa022d8043582b7ebc8da9066dd8e6378a7d6c9d
Maxim Talanov <mail@maxtalanov.ru>
2025-06-15 17:31:38 +0300

    feat (16.6.4): Задан заголовок страницы

--- src/pages/InteriorDesignPage/ui/InteriorDesignPage.tsx
+++ src/pages/InteriorDesignPage/ui/InteriorDesignPage.tsx
@@ -1,11 +1,14 @@
 import { memo } from 'react';
 
+import { useDocumentTitle } from 'shared/lib/hooks/useDocumentTitle/useDocumentTitle';
+
 interface InteriorDesignPageProps {
   className?: string;
 }
 
 const InteriorDesignPage = memo((props: InteriorDesignPageProps) => {
   const { className } = props;
+  useDocumentTitle('Дизайн-проект интерьера');
 
   return <>PAGE: InteriorDesignPage</>;
 });
