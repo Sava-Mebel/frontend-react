a3d4bd903589a8617eec0e0300cf4c928c1881bb
Maxim Talanov <mail@maxtalanov.ru>
2025-06-24 03:34:24 +0300

    ref(19.4): рандомные иконки для каталогов

--- src/features/CatalogGroup/ui/Card/Card.tsx
+++ src/features/CatalogGroup/ui/Card/Card.tsx
@@ -1,4 +1,6 @@
-import { classNames, Mods } from 'shared/lib/classNames/classNames';
+import { useEffect, useState } from 'react';
+
+import { classNames } from 'shared/lib/classNames/classNames';
 import { Badge, BadgeTypes } from 'shared/ui/Badge/Badge';
 import { AppLink, AppLinkVariant } from 'shared/ui/AppLink';
 
@@ -10,25 +12,32 @@ interface CardProps {
   toPaht?: string;
 }
 
+const arrImg: string[] = [
+  'https://raw.githubusercontent.com/Sava-Mebel/images/refs/heads/main/.idea/card/2.png',
+  // ... остальные URL из массива
+  'https://raw.githubusercontent.com/Sava-Mebel/images/refs/heads/main/.idea/card/22.png',
+];
+
 export const Card = (props: CardProps) => {
   const { className, badge, toPaht = '/catalog/kitchen-sets/1' } = props;
 
-  const modsBadge: Mods = badge ? { [cls[badge]]: true } : {};
+  const [randomImage, setRandomImage] = useState<string>('');
+
+  useEffect(() => {
+    const index = Math.floor(Math.random() * arrImg.length);
+    setRandomImage(arrImg[index]);
+  }, []); // пустой массив зависимостей — сработает один раз при монтировании
 
   return (
     <AppLink variant={AppLinkVariant.ROUTE} to={toPaht} className={cls.AppLink}>
       <figure className={classNames(cls.Card, {}, [className])}>
         {badge && (
-          <span className={classNames(cls.badge, modsBadge)}>
+          <span className={classNames(cls.badge, { [cls[badge]]: true })}>
             <Badge mods={badge} />
           </span>
         )}
 
-        <img
-          className={cls.img}
-          src={'https://www.arboro.de/blog/wp-content/uploads/2010/05/img-tag.jpg'}
-          alt="img"
-        />
+        <img className={cls.img} src={randomImage} alt="img" />
         <figcaption className={cls.header}>
           <h3 className={cls.title}>Заголовок для карточки</h3>
           <p className={cls.description}>Изготовление индивидуальной мебели на заказ</p>
