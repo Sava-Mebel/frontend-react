2428241c8371f48cad52c856cbb3d4733f05e101
Maxim Talanov <mail@maxtalanov.ru>
2025-06-26 05:09:55 +0300

    feat(20.1): GH actions (test)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -18,6 +18,7 @@ function App() {
 
   return (
     <div className={'app'}>
+      тест
       <Header theme={headerMode} />
       <main className={`app__main ${footerMode === 'fixed' ? 'with-fixed-footer' : ''}`}>
         <Suspense fallback={null}>
