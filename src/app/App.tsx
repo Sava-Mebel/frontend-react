1142ea802660fe4ac910c398bac517135f40e218
Maxim Talanov <mail@maxtalanov.ru>
2025-06-26 06:26:08 +0300

    feat(20.6): GH actions (test) - проверка отработки

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,9 +19,6 @@ function App() {
   return (
     <div className={'app'}>
       <Header theme={headerMode} />
-      <Header theme={headerMode} />
-      <Header theme={headerMode} />
-      <Header theme={headerMode} />
       <main className={`app__main ${footerMode === 'fixed' ? 'with-fixed-footer' : ''}`}>
         <Suspense fallback={null}>
           <AppRouter />
